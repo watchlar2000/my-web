a419e079d2ec1ef74f1eb04ba2a610e4ee9c864d
watchlar2000 <sergii.koch@gmail.com>
2025-09-21 16:31:55 +0200

    feat: + add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import config from 'eslint-config-watchlar2000-js';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  ...config,
+  prettierConfig,
+  {
+    files: ['.eleventy.js', 'eleventy.config.js', '_11ty/**/*.js'],
+    rules: {
+      'no-magic-numbers': 'off',
+      'unicorn/prefer-module': 'off',
+    },
+  },
+  {
+    ignores: [
+      '_site/**',
+      'node_modules/**',
+      '**/*.njk',
+      '**/*.html',
+      'public/**',
+      '.cache/**',
+    ],
+  },
+];
